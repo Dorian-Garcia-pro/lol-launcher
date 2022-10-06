0f1cc343c72e0ce51e460ee264f5fedaf5af670c
Dorian-Garcia-pro <garcia.dorian.pro@gmail.com>
2022-10-06 13:49:32 +0200

    WIP screen

--- src/pages/TFT.js
+++ src/pages/TFT.js
@@ -3,12 +3,17 @@ import React from "react";
 function TFT() {
   return (
     <div id="tftContainer">
-      <div class="lds-ring">
+      {/*       <div class="lds-ring">
         <div></div>
         <div></div>
         <div></div>
         <div></div>
-      </div>
+      </div> */}
+      <img
+        src="https://img-19.ccm2.net/oVNTiC26HYCOJDvvrTeVJTFinkk=/330x330/7f2127524d3d4730bd1cb369f8f20618/auth-avatar/Ccm4BzsqZ"
+        alt=""
+      />
+      <p>Il semble qu'il n'y ait rien à voir ici pour le moment.. </p>
     </div>
   );
 }
